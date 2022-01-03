84210186dcadc7d0abe7cd42fd666dd17f555183
Muhaddis <muhaddisshah@gmail.com>
2022-01-03 06:59:02 +0500

    redeploy test

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 const core = require("@actions/core")
 const github = require("@actions/github")
+const { Octokit } = require("@octokit/action")
 
 try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   //   console.log(`The event payload: ${payload}`)
 
-  const github_token = core.getInput("GITHUB_TOKEN")
-  const octokit = github.getOctokit(github_token)
+  const octokit = new Octokit()
 
   let due_date = new Date("30 January 2021")
 
-  octokit.rest.issues
-    .createMilestone({
+  const { data } = octokit
+    .request("POST /repos/foretheta/devops/milestones", {
       owner: github.context.repo.owner,
       repo: "devops",
       title: "Sprint(9/11)-A",
       due_on: due_date.toISOString(),
     })
-    .then((data) => {
-      console.log("Done")
+    .then(() => {
+      console.log("Milestone Created: %s", data.html_url)
     })
-    .catch((error) => {
-      core.setFailed(JSON.stringify(error))
+    .error(() => {
+      console.log("error")
     })
 } catch (error) {
   core.setFailed(error.message)
