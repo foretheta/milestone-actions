ca9dfcb7963ba577ae73f2d5b0e464bbe620c255
Muhaddis <muhaddisshah@gmail.com>
2022-01-03 07:21:28 +0500

    redeploy test

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
 const core = require("@actions/core")
 const github = require("@actions/github")
-const { Octokit } = require("@octokit/action")
+const { Octokit } = require("@octokit/core")
 
-async function run() {
-  try {
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    //   console.log(`The event payload: ${payload}`)
+try {
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  //   console.log(`The event payload: ${payload}`)
 
-    const octokit = new Octokit()
+  const github_token = core.getInput("GITHUB_TOKEN")
+  const octokit = new Octokit({ auth: github_token })
 
-    let due_date = new Date("30 January 2021")
+  const response = octokit
+    .request("GET /orgs/foretheta/repos", {
+      org: "foretheta",
+      type: "private",
+    })
+    .then((text) => {
+      console.log(JSON.stringify(text))
+    })
+    .catch((err) => {
+      console.log(JSON.stringify(err))
+    })
 
-    const { data } = await octokit.request(
-      "POST /repos/foretheta/required-labels/milestones",
-      {
-        owner: github.context.repo.owner,
-        repo: "required-labels",
-        title: "Sprint(9/11)-A",
-        due_on: due_date.toISOString(),
-      }
-    )
+  let due_date = new Date("30 January 2021")
 
-    console.log("Milestone Created: %s", data)
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+  //   octokit.request
+  //     .createMilestone({
+  //       owner: "foretheta",
+  //       repo: "devops",
+  //       title: "Sprint(9/11)-A",
+  //       due_on: due_date.toISOString(),
+  //     })
+  //     .then((data) => {
+  //       console.log("Done")
+  //     })
+  //     .catch((error) => {
+  //       core.setFailed(JSON.stringify(error))
+  //     })
+} catch (error) {
+  core.setFailed(error.message)
 }
-
-run()
