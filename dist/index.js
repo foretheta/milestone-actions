e20fa51319942f82344065b10dd9549f94435a2b
Mazhar Hussain <42507791+Maxhar30@users.noreply.github.com>
2022-08-25 10:21:32 +0500

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -8471,14 +8471,14 @@ try {
   octokit.rest.issues.createMilestone({
     owner: github.context.repo.owner,
     repo: github.context.repo.repo,
-    title: "Sprint(" + currentMonth + "/" + currentYear + ")-a",
+    title: "Sprint (" + currentMonth + "/" + currentYear + ")-a",
     due_on: MID_OF_THE_MONTH_DATE.toISOString(),
   })
 
   octokit.rest.issues.createMilestone({
     owner: github.context.repo.owner,
     repo: github.context.repo.repo,
-    title: "Sprint(" + currentMonth + "/" + currentYear + ")-b",
+    title: "Sprint (" + currentMonth + "/" + currentYear + ")-b",
     due_on: LAST_DAY_OF_THE_MONTH_DATE.toISOString(),
   })
 } catch (error) {
